358c018401924cac8070cf1886bc23c38fe44586
David Connolly <david@volthauslab.com>
2017-01-31 02:00:55 +0000

    All you need to complete this project
    The apk file is the one to install to your Android phone. The aia file is the one you will want to import into your MIT App Inventor interface to edit to your own wizardly desires. have fun and be SAFE!

---
+++ bluetoothLEDcontrol.ino
@@ -0,0 +1,41 @@
+/* Bluetooth - Arduino Remote LED Control
+Downloaded from Volthaus Electronics Laboratory Austin Texas
+http://volthauslab.com
+*/
+
+
+
+int ledPin = 13;
+int state = 0;
+int flag = 0;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+
+  Serial.begin(9600); // Default connection rate for my BT module
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+    state = Serial.read();
+    flag = 0;
+  }
+
+  if (state == '0') {
+    digitalWrite(ledPin, LOW);
+    if (flag == 0) {
+      Serial.println("LED: off");
+      flag = 1;
+    }
+  }
+
+  else if (state == '1') {
+    digitalWrite(ledPin, HIGH);
+    if (flag == 0) {
+      Serial.println("LED: on");
+      flag = 1;
+    }
+  }
+}
